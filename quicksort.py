b643167d032ed32eed6755e5b319bc9b84f0ab40
tpabasra <115719186+tpabasra@users.noreply.github.com>
2022-10-13 19:41:19 +0530

    Create quicksort.py

---
+++ quicksort.py
@@ -0,0 +1,46 @@
+
+def partition(array, low, high):
+ 
+ 
+    pivot = array[high]
+ 
+    i = low - 1 
+
+    for j in range(low, high):
+        if array[j] <= pivot:
+ 
+            i = i + 1
+
+            (array[i], array[j]) = (array[j], array[i])
+ 
+    (array[i + 1], array[high]) = (array[high], array[i + 1])
+ 
+    return i + 1
+ 
+ 
+ 
+def quickSort(array, low, high):
+    if low < high:
+ 
+        # Find pivot element such that
+        # element smaller than pivot are on the left
+        # element greater than pivot are on the right
+        pi = partition(array, low, high)
+ 
+        # Recursive call on the left of pivot
+        quickSort(array, low, pi - 1)
+ 
+        # Recursive call on the right of pivot
+        quickSort(array, pi + 1, high)
+ 
+ 
+data = [12, 72, 42, 12, 10, 9, -2]
+print("Unsorted Array")
+print(data)
+ 
+size = len(data)
+ 
+quickSort(data, 0, size - 1)
+ 
+print('Sorted Array in Ascending Order:')
+print(data)
